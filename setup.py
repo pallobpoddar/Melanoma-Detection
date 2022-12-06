7113323671240cff2f474bfb5b91b061d78b5652
Pallob Poddar <51057880+PallobPoddar@users.noreply.github.com>
2022-12-06 12:13:44 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name = 'wtfml',
+  version = '1.1',
+  license = '',
+  author = 'pallob',
+  author_email = 'pallob.poddar@northsouth.edu',
+  description = 'wtfml package'
+  
